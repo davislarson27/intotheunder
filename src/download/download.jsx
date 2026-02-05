c1ede35b0184f5f316ca5783fc46b7c48e1d020d
Davis Larson <davislarson27@outlook.com>
2026-02-05 12:41:52 -0700

    moved download.html to .jsx

--- src/download/download.jsx
+++ src/download/download.jsx
@@ -2,8 +2,58 @@ import React from 'react';
 
 export function Download() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>download displayed here</div>
+    <main class="py-4 flex-grow-1">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-12 col-md-6">
+                    <div class="card shadow-sm">
+                        <div class="card-header">
+                            <h4 class="h4 mb-0">Download App</h4>
+                        </div>
+                        <div class="card-body">
+                            <p>
+                                <label class="form-label">Select your computer type:</label>
+                                <select class="form-select">
+                                    <option>Mac (Silicon)</option>
+                                    <option>Windows</option>
+                                </select>
+                            </p>
+                            <p>
+                                <label class="form-label">Select Game Version</label>
+                                <select class="form-select">
+                                    <optgroup label="1.4">
+                                        <option>v1.4.0 (current)</option>
+                                    </optgroup>
+                                    <optgroup label="1.3">
+                                        <option>v1.3.2</option>
+                                        <option>v1.3.1</option>
+                                        <option>v1.3.0</option>    
+                                    </optgroup>
+                                </select>
+                            </p>
+                            <button class="btn btn-primary w-100 mt-3">Download</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="card shadow-sm">
+                        <div class="card-header">
+                            <h4  class="h4 mb-0">Version Release Notes</h4>
+                        </div>
+                        <div class="card-body">
+                            <p><span id="selected_game_version" class="text-muted">v1.4.0</span> <span id="selected_game_version_name">The Arctic Update!</span></p>
+                            <ul>
+                                <li>added glacier biome</li>
+                                <li>added naturally generating snowmen</li>
+                                <li>changed key to build blocks to right click</li>
+                                <li>changed key to destroy blocks to left click</li>
+                                <li>added chests</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
-  );
+);
 }
