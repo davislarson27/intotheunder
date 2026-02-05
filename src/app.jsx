6ee62c1239b2f7fc8209a846c9dfffc591684843
Davis Larson <davislarson27@outlook.com>
2026-02-05 12:57:18 -0700

    fixed footer problems

--- src/app.jsx
+++ src/app.jsx
@@ -17,61 +17,62 @@ import { NotFound } from './notfound/notfound';
 export default function App() {
   return (
     <BrowserRouter>
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid px-4">
-                    <NavLink className="navbar-brand" to="home">Into The Under</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>                  
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <div className="d-flex ms-lg-auto">
-                            <ul className="navbar-nav mb-2 mb-lg-0">
-                                <li className="nav-item"><NavLink to="home" className="nav-link">Home</NavLink></li>
-                                <li className="nav-item"><NavLink to="download" href="download.html" className="nav-link">Download</NavLink></li>
-                                <li className="nav-item"><NavLink to="feedback" href="feedback.html" className="nav-link">Feedback</NavLink></li>
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
-                                    
-                                    <ul className="dropdown-menu dropdown-menu-end">
-                                        <li><NavLink className="dropdown-item" to="login">Log In</NavLink></li>
-                                        <li><NavLink className="dropdown-item" to="sign_up">Sign Up</NavLink></li>
-                                        <li><NavLink className="dropdown-item" to="profile">Profile</NavLink></li>
-                                    </ul>
-                                    
-                                </li>
-                            </ul>
+        <div className="d-flex flex-column min-vh-100">
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid px-4">
+                        <NavLink className="navbar-brand" to="home">Into The Under</NavLink>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>                  
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <div className="d-flex ms-lg-auto">
+                                <ul className="navbar-nav mb-2 mb-lg-0">
+                                    <li className="nav-item"><NavLink to="home" className="nav-link">Home</NavLink></li>
+                                    <li className="nav-item"><NavLink to="download" href="download.html" className="nav-link">Download</NavLink></li>
+                                    <li className="nav-item"><NavLink to="feedback" href="feedback.html" className="nav-link">Feedback</NavLink></li>
+                                    <li className="nav-item dropdown">
+                                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
+                                        
+                                        <ul className="dropdown-menu dropdown-menu-end">
+                                            <li><NavLink className="dropdown-item" to="login">Log In</NavLink></li>
+                                            <li><NavLink className="dropdown-item" to="sign_up">Sign Up</NavLink></li>
+                                            <li><NavLink className="dropdown-item" to="profile">Profile</NavLink></li>
+                                        </ul>
+                                        
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
+                </nav>
+            </header>
+
+        <Routes>
+            <Route path='/' element={<Home />} exact />
+            <Route path='/home' element={<Home />} />
+            <Route path='/download' element={<Download />} />
+            <Route path='/feedback' element={<Feedback />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/sign_up' element={<Sign_up />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+
+            
+            <footer className="bg-light text-dark">
+                <div className="container py-4">
+                    <div className="row align-items-center g-4">
+                        <div className="col-12 col-md-auto">
+                            <span className="footer_text text-muted">&copy; 2026 Into The Under</span>
+                        </div>
+                        <div className="col-12 col-md-auto">
+                            <a className="text-muted" target="_blank" href="https://github.com/davislarson27/intotheunder/">Page Git Hub</a>
+                        </div>
+                    </div>    
                 </div>
-            </nav>
-        </header>
-
-      <Routes>
-        <Route path='/' element={<Home />} exact />
-        <Route path='/home' element={<Home />} />
-        <Route path='/download' element={<Download />} />
-        <Route path='/feedback' element={<Feedback />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/sign_up' element={<Sign_up />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-
-        
-        <footer className="bg-light text-dark">
-            <div className="container py-4">
-                <div className="row align-items-center g-4">
-                    <div className="col-12 col-md-auto">
-                        <span className="footer_text text-muted">&copy; 2026 Into The Under</span>
-                    </div>
-                    <div className="col-12 col-md-auto">
-                        <a className="text-muted" target="_blank" href="https://github.com/davislarson27/intotheunder/">Page Git Hub</a>
-                    </div>
-                </div>    
-            </div>
-        </footer>
-        
+            </footer>
+        </div>
     </BrowserRouter>
   );
 }
