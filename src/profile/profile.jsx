2bb765cfd6cce9a1d7e16a998cd6f9637dd5279f
Davis Larson <davislarson27@outlook.com>
2026-02-05 12:46:12 -0700

    transitioned profile.html to .jsx

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 export function Profile() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Profile displayed here</div>
+    <main class="py-4 flex-grow-1">
+
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-10 col-md-6 mx-auto">
+
+                    <div class="card shadow-sm">
+                        <div class="card-header">
+                            <h4 class="h-4">Profile Details</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="mb-3">User: <span class="userName_displayed">username123</span></div>
+                            <div class="mb-3">Email: <span class="userEmail_displayed">useremail@gmail.com</span></div>
+                            <div class="mb-3">Latest Version Installed: <span class="latestVersion_displayed">v1.4.0</span></div>
+
+                            <br class="my-3"/>
+
+                            <div class="mb-3">
+                                <button class="btn btn-outline-danger w-100">Log Out</button>
+                            </div>
+
+                        </div>
+                    </div>
+        
+                </div>
+            </div>
+        </div>
+
     </main>
-  );
+);
 }
