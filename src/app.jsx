811c10d6f4db2c4e27fdd8c9decb40906b25204a
Davis Larson <davislarson27@outlook.com>
2026-02-12 15:58:51 -0700

    started creating global props, moved comments to be a global prop that is obtained from service.js

--- src/app.jsx
+++ src/app.jsx
@@ -16,99 +16,74 @@ import { Sign_up } from './sign-up/sign-up';
 import { Profile } from './profile/profile';
 import { NotFound } from './notfound/notfound';
 
+import { getComments } from './service';
+
 
 
 export default function App() {
-  return (
-    <BrowserRouter>
-        <div className="d-flex flex-column min-vh-100">
-            <header>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid px-4">
-                        <NavLink className="navbar-brand" to="/">Into The Under</NavLink>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>                  
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <div className="d-flex ms-lg-auto">
-                                <ul className="navbar-nav mb-2 mb-lg-0">
-                                    <li className="nav-item"><NavLink to="/" className="nav-link">Home</NavLink></li>
-                                    <li className="nav-item"><NavLink to="download" className="nav-link">Download</NavLink></li>
-                                    <li className="nav-item"><NavLink to="feedback" className="nav-link">Feedback</NavLink></li>
-                                    <li className="nav-item dropdown">
-                                        <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
-                                        
-                                        <ul className="dropdown-menu dropdown-menu-end">
-                                            <li><NavLink className="dropdown-item" to="login">Log In</NavLink></li>
-                                            <li><NavLink className="dropdown-item" to="sign-up">Sign Up</NavLink></li>
-                                            <li><NavLink className="dropdown-item" to="profile">Profile</NavLink></li>
-                                        </ul>
-                                        
-                                    </li>
-                                </ul>
+
+    const [userData, changeUserData] = React.useState (null);
+    const [dbComments, updateDbComments] = React.useState (getComments());
+    
+
+    return (
+        <BrowserRouter>
+            <div className="d-flex flex-column min-vh-100">
+                <header>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <div className="container-fluid px-4">
+                            <NavLink className="navbar-brand" to="/">Into The Under</NavLink>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>                  
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <div className="d-flex ms-lg-auto">
+                                    <ul className="navbar-nav mb-2 mb-lg-0">
+                                        <li className="nav-item"><NavLink to="/" className="nav-link">Home</NavLink></li>
+                                        <li className="nav-item"><NavLink to="download" className="nav-link">Download</NavLink></li>
+                                        <li className="nav-item"><NavLink to="feedback" className="nav-link">Feedback</NavLink></li>
+                                        <li className="nav-item dropdown">
+                                            <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
+                                            
+                                            <ul className="dropdown-menu dropdown-menu-end">
+                                                <li><NavLink className="dropdown-item" to="login">Log In</NavLink></li>
+                                                <li><NavLink className="dropdown-item" to="sign-up">Sign Up</NavLink></li>
+                                                <li><NavLink className="dropdown-item" to="profile">Profile</NavLink></li>
+                                            </ul>
+                                            
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
+                    </nav>
+                </header>
+
+            <Routes>
+                <Route path='/' element={<Home />} exact />
+                <Route path='/home' element={<Home />} />
+                <Route path='/download' element={<Download />} />
+                <Route path='/feedback' element={<Feedback dbComments={dbComments} updateDbComments={updateDbComments} />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/sign-up' element={<Sign_up />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+
+                
+                <footer className="bg-light text-dark">
+                    <div className="container py-4">
+                        <div className="row align-items-center g-4">
+                            <div className="col-12 col-md-auto">
+                                <span className="footer_text text-muted">&copy; 2026 Into The Under</span>
+                            </div>
+                            <div className="col-12 col-md-auto">
+                                <a className="text-muted" target="_blank" href="https://github.com/davislarson27/intotheunder/">Page Git Hub</a>
+                            </div>
+                        </div>    
                     </div>
-                </nav>
-            </header>
-
-        <Routes>
-            <Route path='/' element={<Home />} exact />
-            <Route path='/home' element={<Home />} />
-            <Route path='/download' element={<Download />} />
-            <Route path='/feedback' element={<Feedback />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/sign-up' element={<Sign_up />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='*' element={<NotFound />} />
-        </Routes>
-
-            
-            <footer className="bg-light text-dark">
-                <div className="container py-4">
-                    <div className="row align-items-center g-4">
-                        <div className="col-12 col-md-auto">
-                            <span className="footer_text text-muted">&copy; 2026 Into The Under</span>
-                        </div>
-                        <div className="col-12 col-md-auto">
-                            <a className="text-muted" target="_blank" href="https://github.com/davislarson27/intotheunder/">Page Git Hub</a>
-                        </div>
-                    </div>    
-                </div>
-            </footer>
-        </div>
-    </BrowserRouter>
-  );
-}
-
-function DataBaseStructureMock () {
-    const [userData, changeUserData] = React.useState (
-        {
-            userName: "userName123",
-            passwordToken: "tokenofsomeform",
-            userEmail: "userEmail@email.com",
-            lastVersionDownloaded: "v1.4.0",
-            lastOSDownloaded: "macsilicon",
-            userCommentsIDs: [ // may want to figure out sets at some point for this
-                11,
-                12    
-            ],
-
-            // maybe store list of commentIDs made by user
-        }
-    )
-
-    const [comments, changeComments] = React.useState (
-        [
-            {
-                commentID: 1,
-                user: "BestUserNameEver",
-                commentVersion: "v1.4.0",
-                commentText: "The game is cool but crafting unique blocks would make it even better",
-                likes: 87,
-                isLikedByUser: false // server figures out whether the user has liked the comment
-                // when user likes: call function that likes on the page, then tells the server it was liked. it validates.
-            },
-        ]
-    )
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
 }
--- src/service.js
+++ src/service.js
@@ -95,3 +95,136 @@ export function logInUser (userEmail, password) {
         return null;
     }
 }
+
+export function getComments () {
+    return [
+        {
+            commentID: 1,
+            user: "BestUserNameEver",
+            commentVersion: "v1.4.0",
+            commentText: "The game is cool but crafting unique blocks would make it even better",
+            likes: 87
+        },
+        {
+            commentID: 2,
+            user: "JimTheThird",
+            commentVersion: "v1.4.0",
+            commentText: "We should let us remove stuff from our inventory or throw it out",
+            likes: 62
+        },
+        {
+            commentID: 3,
+            user: "CrazyDave67",
+            commentVersion: "v1.3.2",
+            commentText: "I wish that we had chests in the game",
+            likes: 26
+        },
+        {
+            commentID: 4,
+            user: "McDonaldsLover",
+            commentVersion: "v1.4.0",
+            commentText: "Increase the build height limit!",
+            likes: 17
+        },
+        {
+            commentID: 5,
+            user: "JoseTheGOAT",
+            commentVersion: "v1.4.0",
+            commentText: "A background would be so cool! Maybe like in Terraria.",
+            likes: 17
+        },
+        {
+            commentID: 6,
+            user: "McDonaldsLover",
+            commentVersion: "v1.3.1",
+            commentText: "Ok imagine this. Right now the insides of buildings look off because they don't have backgrounds. Imagine if they had a way to add blocks as a wall that made it look more like you were inside! I would be so down for that. Then we wouldn't have to dig out the ground to build tall stuff!",
+            likes: 14
+        },
+        {
+            commentID: 7,
+            user: "Johnny123",
+            commentVersion: "v1.3.0",
+            commentText: "Doors please?",
+            likes: 11
+        },
+        {
+            commentID: 8,
+            user: "rambo2.0",
+            commentVersion: "v1.3.1",
+            commentText: "Please make swimming mechanics more consistent!",
+            likes: 9
+        },
+        {
+            commentID: 9,
+            user: "WhatAUser111",
+            commentVersion: "v1.3.1",
+            commentText: "Ladders would be so nice",
+            likes: 5
+        },
+        {
+            commentID: 10,
+            user: "MegaBatman",
+            commentVersion: "v1.4.0",
+            commentText: "Please add the ability to take damage!",
+            likes: 3
+        },
+        {
+            commentID: 11,
+            user: "SomeoneCool",
+            commentVersion: "v1.4.0",
+            commentText: "Here's some bad advice",
+            likes: 2
+        },
+        {
+            commentID: 12,
+            user: "User123",
+            commentVersion: "v1.4.0",
+            commentText: "I like cheese",
+            likes: 2
+        },
+        {
+            commentID: 13,
+            user: "TheFakeJimmer",
+            commentVersion: "v1.4.0",
+            commentText: "you should add the whole energy bar thing",
+            likes: 1
+        },
+        {
+            commentID: 14,
+            user: "MabelMagnet",
+            commentVersion: "v1.4.0",
+            commentText: "Multiplayer would be cool",
+            likes: 1
+        },
+        {
+            commentID: 15,
+            user: "JasonBourne123",
+            commentVersion: "v1.4.0",
+            commentText: "what about adding a hunger bar?",
+            likes: 0
+        },
+        {
+            commentID: 16,
+            user: "NotAPolitician",
+            commentVersion: "v1.4.0",
+            commentText: "Can we choose between different skins",
+            likes: 0
+        },
+        {
+            commentID: 17,
+            user: "TheJoker",
+            commentVersion: "v1.4.0",
+            commentText: "We should have creative mode like in Minecraft",
+            likes: 0
+        },
+        {
+            commentID: 18,
+            user: "CougarsBoulevard",
+            commentVersion: "v1.4.0",
+            commentText: "We should be able to play on Linux",
+            likes: 0
+        },
+
+    ];
+
+}
