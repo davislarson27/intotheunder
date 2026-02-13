f06b5f893dc3cd9f80a436d5971f036ac3118ab1
Davis Larson <davislarson27@outlook.com>
2026-02-12 17:00:04 -0700

    filled in profile details

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,35 +1,55 @@
 import React from 'react';
 
 export function Profile({userData, changeUserData}) {
-  return (
-    <main className="py-4 flex-grow-1">
 
-        <div className="container">
-            <div className="row g-4">
-                <div className="col-10 col-md-6 mx-auto">
+    function getVersion (version) {
+        if (version == null) {
+            return "Not Yet Downloaded";
+        } else {
+            return version;
+        }
+    }
+    
+    return (
+        <main className="py-4 flex-grow-1">
 
-                    <div className="card shadow-sm">
-                        <div className="card-header">
-                            <h4 className="h-4">Profile Details</h4>
-                        </div>
-                        <div className="card-body">
-                            <div className="mb-3">User: <span className="userName_displayed">username123</span></div>
-                            <div className="mb-3">Email: <span className="userEmail_displayed">useremail@gmail.com</span></div>
-                            <div className="mb-3">Latest Version Installed: <span className="latestVersion_displayed">v1.4.0</span></div>
-
-                            <br className="my-3"/>
-
-                            <div className="mb-3">
-                                <button className="btn btn-outline-danger w-100">Log Out</button>
+            <div className="container">
+                <div className="row g-4">
+                    <div className="col-10 col-md-6 mx-auto">
+                        <div className="card shadow-sm">
+                            <div className="card-header">
+                                <h4 className="h-4">Profile Details</h4>
                             </div>
 
+                            {userData == null ? (
+                                <>
+                                    <div className="card-body">
+                                        <div className="mb-3">No User Is Logged In</div>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="card-body">
+
+                                        <div className="mb-3">User: {userData.userName}</div>
+                                        <div className="mb-3">Email: {userData.userEmail}</div>
+                                        <div className="mb-3">Latest Version Installed: {getVersion(userData.lastVersionDownloaded)}</div>
+
+                                        <br className="my-3"/>
+
+                                        <div className="mb-3">
+                                            <button className="btn btn-outline-danger w-100">Log Out</button>
+                                        </div>
+
+                                    </div>
+                                </>
+                            )}
                         </div>
+            
                     </div>
-        
                 </div>
             </div>
-        </div>
 
-    </main>
-);
+        </main>
+    );
 }
